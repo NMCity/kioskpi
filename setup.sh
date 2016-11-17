d46969d6a9effc609a68b6f5692672539cdd896f
Christopher Pirner <christopher_pirner@hotmail.com>
2016-11-17 17:44:30 +0100

    changes

--- setup.sh
+++ setup.sh
@@ -144,6 +144,9 @@ function install_auto_load_openbox {
                 echo "" >> /etc/rc.local
                 echo "exit 0" >> /etc/rc.local
                 echo "" >> /etc/rc.local
+
+                chmod +x /etc/rc.load
+                
                 echo -e "\e[1;91m$window_manger now loads on boot\e[0m"
         fi        
 }
